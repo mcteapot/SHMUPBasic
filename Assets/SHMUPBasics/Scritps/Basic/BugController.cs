e4ac6b0e25d63203810e3f5f8f219bccdfe7ad9c
Arjun Prakash <archieoi@gmail.com>
2016-10-03 11:17:11 -0700

    bug moving across screen, with trigger working

---
+++ Assets/SHMUPBasics/Scritps/Basic/BugController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BugController : MonoBehaviour {
+
+	public float bugSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.down * Time.deltaTime * bugSpeed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		//Destroy(other.gameObject);
+
+		Debug.Log("TRIGGER WORKING");
+		Destroy(gameObject);
+
+	}
+}
