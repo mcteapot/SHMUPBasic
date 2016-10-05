9d65722919f140cb24787837ffe306bf9d542058
Arjun Prakash <archieoi@gmail.com>
2016-10-05 09:50:57 -0700

    big swan on timer

--- Assets/SHMUPBasics/Scritps/Basic/BugSpawn.cs
+++ Assets/SHMUPBasics/Scritps/Basic/BugSpawn.cs
@@ -5,14 +5,34 @@ public class BugSpawn : MonoBehaviour {
 
 	public GameObject bugPrefab;
 
+	private IEnumerator coroutine;
+
+	private bool SpanIsRunning = false;
 	// Use this for initialization
 	void Start () {
-	
+		coroutine = WaitAndSpawnBug(5.0f);
+		StartCoroutine(coroutine);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Instantiate(bugPrefab, transform.position, transform.rotation);
+		
+		if(SpanIsRunning == false)
+		{
+			coroutine = WaitAndSpawnBug(2.0f);
+			StartCoroutine(coroutine);
+		}
+
 	
+	}
+
+	private IEnumerator WaitAndSpawnBug(float waitTime) {
+		SpanIsRunning = true;
+		Debug.Log("Wait HERE");
+		yield return new WaitForSeconds(waitTime);
+		Instantiate(bugPrefab, transform.position, transform.rotation);
+		Debug.Log("SPAWN BUG");
+		SpanIsRunning = false;
+
 	}
 }
